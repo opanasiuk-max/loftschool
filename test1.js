ac718307873e1d1b321f3047a889087edcc87acc
opanasiuk-max <maxhill@ukr.net>
2017-01-11 14:47:24 +0200

    test work

---
+++ test1.js
@@ -0,0 +1,11 @@
+//Тестовое задание 1
+
+var a=2;
+//Math.floor(Math.random(1, 100));
+
+var b=Math.floor(Math.random() * 100)+1
+console.log(b);
+var rez=a+b;
+
+document.write(a+'+'+b+'='+rez);
+
---
+++ test2.js
@@ -0,0 +1,9 @@
+//Тестовое задание 2
+
+var arr=[];
+for (var i=1; i<11; i++){
+    if (i%2==0){
+        arr.push(i);
+    }
+}
+document.write(arr);
